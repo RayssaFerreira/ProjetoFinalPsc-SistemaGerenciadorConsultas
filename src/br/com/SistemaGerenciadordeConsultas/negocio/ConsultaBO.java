51f7470c9c68d07548609a5e8df6960f8bf4aa91
RayssaFerreira <rayssa_ramosf@hotmail.com>
2016-07-26 15:57:47 -0300

    Modificações

---
+++ src/br/com/SistemaGerenciadordeConsultas/negocio/ConsultaBO.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.SistemaGerenciadordeConsultas.negocio;
+
+import br.com.SistemaGerenciadordeConsultas.entidade.Consulta;
+import br.com.SistemaGerenciadordeConsultas.entidade.Medico;
+import br.com.SistemaGerenciadordeConsultas.entidade.Paciente;
+import br.com.SistemaGerenciadordeConsultas.entidade.QuantidadeConsultaPorDia;
+import br.com.SistemaGerenciadordeConsultas.excecao.CampoObrigatorioException;
+import br.com.SistemaGerenciadordeConsultas.excecao.DataInvalidaException;
+import br.com.SistemaGerenciadordeConsultas.persistencia.ConsultaDAO;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Rayssa
+ */
+public class ConsultaBO {
+
+    public List<Consulta> buscarTodosPorPaciente(Paciente paciente) throws SQLException {
+        ConsultaDAO consultaDAO = new ConsultaDAO();
+        return consultaDAO.buscarPorPaciente(paciente);
+    }
+    public List<Consulta> buscarTodosPorMedico(Medico medico) throws SQLException {
+        ConsultaDAO consultaDAO = new ConsultaDAO();
+        return consultaDAO.buscarPorMedico(medico);
+    }
+    public List<Consulta> buscarTodos() throws SQLException {
+        ConsultaDAO consultaDAO = new ConsultaDAO();
+        return consultaDAO.buscarTodos();
+    }
+
+    public void salvar(Consulta consulta) throws SQLException {
+        ConsultaDAO consultaDAO = new ConsultaDAO();
+        consultaDAO.criar(consulta);
+    }
+     private void validaData(String data) throws ParseException {
+        Date dataAtual = new Date(System.currentTimeMillis());
+        Date dataInformada = ConverteData.StringForDate(data);
+
+        Date dataAtualZera = zeraHoras1(dataAtual);
+
+        if (dataInformada.before(dataAtualZera)) {
+            throw new DataInvalidaException("Data inválida");
+        }
+
+    }
+
+    private static Date zeraHoras1(Date data) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(data);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return (Date) cal.getTime();
+
+    }
+
+    public void editar(Consulta consulta) throws SQLException {
+        ConsultaDAO consultaDAO = new ConsultaDAO();
+        consultaDAO.alterar(consulta);
+    }
+    
+    public List<QuantidadeConsultaPorDia> buscarConsultasPorDia() throws SQLException {
+        ConsultaDAO consultaDAO = new ConsultaDAO();
+        return consultaDAO.buscarConsultasPorDia();
+    }
+    public void removerConsulta(int id) throws SQLException {
+        ConsultaDAO consultaDAO = new ConsultaDAO();
+        consultaDAO.removerConsulta(id);
+    }
+
+   
+}
